ab6ad7d096743eeb4bbaa3ea56a43c15d82a44ef
henrynascimentosilva <henrynascimentosilva@gmail.com>
2024-10-12 05:01:03 -0300

    contato

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,8 +1,17 @@
 function Footer() {
   return (
     <div className="shadow-md w-full bottom-auto left-0 font-poppins">
-      <div className="md:flex self-center justify-center mt-10 py-5 bg-slate-100">
-        Cadastur 53.470.567/0001-98
+      <div className="md:flex justify-around mt-10 py-5 bg-slate-100">
+        <div>
+          Cadastur 53.470.567/0001-98
+        </div>
+        <div>
+          <ul className="flex">
+            <li><ion-icon name="logo-facebook"></ion-icon></li>
+            <li className="mx-4"><ion-icon name="logo-instagram"></ion-icon></li>
+            <li><ion-icon name="logo-whatsapp"></ion-icon></li>
+          </ul>
+        </div>
       </div>
     </div>
   );
